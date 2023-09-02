3f7113075661a2c8b3b798cd2fe36ed10d3195f9
Ahmed Shaaban <ahmedlearningscience@gmail.com>
2023-09-02 19:17:13 +0300

    Add Regex validation - Readable code - Maintanable code

--- js/main.js
+++ js/main.js
@@ -2,46 +2,41 @@ const screen = document.querySelector("#calculation-screen");
 const errorScreen = document.querySelector("#error-screen");
 const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
-const dot = document.querySelector("#dot");
 const commands = document.querySelectorAll(".command");
 const clearAll = document.querySelector("#clear-all");
 const clearLast = document.querySelector("#clear-last");
 const calculate = document.querySelector("#calculate");
-
-const isOperator = (elem) => {
-    for (operator of ['+', '-', '*', '/', '**']) {
-        if (elem == operator) { return true; }
-    }
-    return false;
-};
+const btns = document.querySelectorAll(".btn");
 
 const precedence = (operator) => {
     if (operator == '+' || operator == '-') {
         return 1;
     } else if (operator == '*' || operator == '/') {
         return 2;
-    } else if (operator == '**') {
+    } else if (operator == '^') {
         return 3;
     }
     return 0;
 };
 
-const strToInfix = (str) => {
-    let token = "";
-    let infix = [];
-    for (val of str) {
-        if (isFinite(val) || val === '.' || val === "e" || token[token.length - 1] === "e") {
-            token += val;
-        } else if (val === "-" && !token) { // Negative Numbers
-            token += '-';
+const validateInput = (str) => {
+    const validNumber = /^(\-|\+)?(\d+\.?\d*|\d*\.\d+)(e(\-|\+)?\d+)?$/;
+    const validOperator = /^[-+*/^]$/;
+    const arr = str.split(" ");
+    if (arr.length % 2 != 1) { return false; } // # of operators + # of numbers = odd -- 2+3
+    for (let i = 0; i < arr.length; i++) {
+        if (i % 2 === 0) {
+            if (!validNumber.test(arr[i])) { return false; }
         } else {
-            infix.push(token);
-            (val === '^') ? infix.push('**') : infix.push(val);
-            token = "";
+            if (!validOperator.test(arr[i])) { return false; }
         }
     }
-    if (token) { infix.push(token); }
-    return infix;
+    return true;
+};
+
+const strToInfix = (str) => {
+    let arr = str.split(" ");
+    return arr;
 }
 
 const infixToPostfix = (infix) => {
@@ -52,22 +47,24 @@ const infixToPostfix = (infix) => {
             postfix.push(val);
             continue;
         }
-        if (val === "**") {
+        if (val === "^") {
             if (precedence(val) >= precedence(stack[stack.length - 1])) {
-                stack.push("**");
+                stack.push(val);
             } else {
                 while (precedence(val) < precedence(stack[stack.length - 1])) {
                     postfix.push(stack.pop());
                 }
                 stack.push(val);
             }
-        } else if (precedence(val) > precedence(stack[stack.length - 1])) {
-            stack.push(val);
         } else {
-            while (precedence(val) <= precedence(stack[stack.length - 1])) {
-                postfix.push(stack.pop());
+            if (precedence(val) > precedence(stack[stack.length - 1])) {
+                stack.push(val);
+            } else {
+                while (precedence(val) <= precedence(stack[stack.length - 1])) {
+                    postfix.push(stack.pop());
+                }
+                stack.push(val);
             }
-            stack.push(val);
         }
     }
     while (stack.length !== 0) {
@@ -88,7 +85,7 @@ const calc = (a, b, operator) => {
             return a * b;
         case '/':
             return a / b;
-        case '**':
+        case '^':
             return a ** b;
         default:
             return null;
@@ -110,83 +107,59 @@ const calcPostfix = (postfix) => {
     return stack[0];
 }
 
-let isDotValid = true;
-
-dot.addEventListener("click", () => {
-    if (isDotValid && screen.textContent.slice(-1) !== '.') {
-        screen.textContent += '.';
-        isDotValid = false;
-    }
+btns.forEach((btn) => {
+    btn.addEventListener("click", () => errorScreen.textContent = "");
 });
 
 numbers.forEach((num) => {
     num.addEventListener("click", () => {
+        screen.value += num.dataset.value;
         errorScreen.textContent = "";
-        screen.textContent += num.dataset.value;
     });
 });
 
 operators.forEach((operator) => {
     operator.addEventListener("click", () => {
-        errorScreen.textContent = "";
-        if (screen.textContent.slice(-1) === '.') { return; }
-        const last = screen.textContent.slice(-1);
-        const preLast = screen.textContent.slice(-2, -1);
-        if (!isOperator(last) && last !== "" && last !== "e") {
-            screen.textContent += operator.dataset.value;
-            isDotValid = true;
-        } else if (operator.dataset.value === "-" && (!last || (preLast && !isOperator(preLast) && preLast !== "e"))) { // Handling negative numbers
-            screen.textContent += "-";
-            isDotValid = true;
+        const opVal = operator.dataset.value;
+        const last = screen.value.slice(-1);
+        // Sign Numbers
+        if ((opVal === "-" || opVal === "+") && (!screen.value || /^[-+*/^] $/.test(screen.value.slice(-2)))) {
+            screen.value += opVal;
+            // e scientific notation
+        } else if (last === "e" && (opVal === "-" || opVal === "+")) {
+            screen.value += opVal;
+        } else {
+            screen.value += ` ${opVal} `;
         }
+        errorScreen.textContent = "";
     });
 });
 
 clearAll.addEventListener("click", () => {
+    screen.value = "";
     errorScreen.textContent = "";
-    screen.textContent = "";
-    isDotValid = true;
 });
 
 clearLast.addEventListener("click", () => {
+    let nDel = 1;
+    if (screen.value.slice(-1) === " ") { nDel = 3 } // if last input is operator
+    screen.value = screen.value.slice(0, -nDel);
     errorScreen.textContent = "";
-    let last = screen.textContent.slice(-1);
-    let preLast = screen.textContent.slice(-2, -1);
-    screen.textContent = screen.textContent.slice(0, -1);
-    if (last === ".") {
-        isDotValid = true;
-    } else if (isOperator(last)) {
-        isDotValid = true;
-        for (let i = screen.textContent.length; i >= 0; --i) {
-            if (isOperator(screen.textContent[i]) && screen.textContent[i - 1] !== "e") { return; }
-            if (screen.textContent[i] === '.' || screen.textContent[i] === 'e') {
-                isDotValid = false;
-                return;
-            }
-        }
-    }
 });
 
 calculate.addEventListener("click", () => {
-    if (!screen.textContent) {
+    if (!screen.value) {
         return;
-    } else if (isNaN(screen.textContent.slice(-1))) {
+    } else if (!validateInput(screen.value)) {
         errorScreen.textContent = "Invalid Operation";
         return;
     }
-    const infix = strToInfix(screen.textContent);
+    const infix = strToInfix(screen.value);
     const postfix = infixToPostfix(infix);
     const result = calcPostfix(postfix);
     if (!isFinite(result)) {
-        errorScreen.textContent = "Infinity";
-    } else {
-        screen.textContent = result;
-        isDotValid = Number.isInteger(result);
-        for (let i = 0; i < screen.textContent.length; ++i) {
-            if (screen.textContent[i] === 'e') {
-                isDotValid = false;
-                break;
-            }
-        }
+        errorScreen.textContent = Infinity;
+        return;
     }
+    screen.value = Number(result);
 });
